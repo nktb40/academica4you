697767cb9e29610f543d1d44989687c716cf6035
Nikita2 <nikita2@nikita-VirtualBox.(none)>
2014-05-11 16:29:52 -0400

    search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require cocoon
+//= require bootstrap-select
+//= require moment
+//= require bootstrap-datetimepicker-ru
+//= require bootstrap-datetimepicker
 //= require turbolinks
-//= require_tree .
